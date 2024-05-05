90ebd867d721c3cc6d1c8f72aa6125b886123026
Calebyte11 <Calebytdembelle11@gmail.com>
2024-05-06 00:18:25 +0100

    Initialized the repo with cookiecutter data science structure for this machine learning project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine learning project which predicts or classifies hand's gestures by reading muscle activity using an Electromyography sensor (e.g MYO armband which has 8 sensors)',
+    author='Caleb Ibekwe',
+    license='MIT',
+)
